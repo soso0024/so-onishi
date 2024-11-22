274bbc4eb6652ab56a0f7d24a8a77e11e6676d54
soso0024 <131151398+soso0024@users.noreply.github.com>
2024-11-23 00:07:44 +0900

    update: add experiences

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -14,8 +14,12 @@ interface Translation {
     passion: string;
     experience: {
       title: string;
-      role: string;
-      description: string;
+      items: {
+        role: string;
+        company: string;
+        period: string;
+        description: string;
+      }[];
     };
   };
   news: {
@@ -84,9 +88,36 @@ export const translations: { [key: string]: Translation } = {
         "I'm passionate about AI and technology, with a focus on solving real-world problems through programming.",
       experience: {
         title: "Experience",
-        role: "Backend Engineer - Mynavi Corporation",
-        description:
-          "Implemented Bookmark-related API functionality during a hackathon",
+        items: [
+          {
+            role: "Frontend Engineer",
+            company: "Yagish Inc.",
+            period: "Oct 2024 ~ Nov 2024",
+            description:
+              "Implemented frontend features for a personal inquiry form in a resume automation service.",
+          },
+          {
+            role: "Backend Engineer",
+            company: "Mynavi Corporation",
+            period: "Sep 2024",
+            description:
+              "Implemented bookmark functionality on the backend and developed an API to retrieve a list of articles bookmarked by users. - App Division 1st Place 🥇",
+          },
+          {
+            role: "Infrastructure Engineer",
+            company: "Chura Data Inc.",
+            period: "Aug 2024",
+            description:
+              "During the internship, developed a search application utilizing RAG (Retrieval-Augmented Generation). Additionally, built infrastructure using AWS, Snowflake, Terraform, and dbt, and visualized data using Snowsight.",
+          },
+          {
+            role: "Data Analyst",
+            company: "Simple Form Inc.",
+            period: "Aug 2024",
+            description:
+              "During the internship, primarily conducted data analysis using Python. Discussed necessary elements for data analysis within the team and reported the results to the manager to share outputs. Through this experience, learned the importance of communication and coordination within the team.",
+          },
+        ],
       },
     },
     news: {
@@ -182,8 +213,36 @@ export const translations: { [key: string]: Translation } = {
         "AIとテクノロジーに情熱を持ち、プログラミングを通じて実世界の問題解決に取り組んでいます。",
       experience: {
         title: "経験",
-        role: "バックエンドエンジニア - マイナビ株式会社",
-        description: "ハッカソンでブックマーク関連のAPI機能を実装",
+        items: [
+          {
+            role: "フロントエンドエンジニア",
+            company: "株式会社Yagish",
+            period: "2024年10月 ~ 11月",
+            description:
+              "履歴書自動化サービスにおける，個人向けのお問い合わせフォームのフロントエンド機能の実装を行いました。",
+          },
+          {
+            role: "バックエンドエンジニア",
+            company: "マイナビ株式会社",
+            period: "2024年9月",
+            description:
+              "バックエンドでのBookmark機能の実装と、ユーザーがBookmarkした記事の一覧を取得するAPIの開発を担当しました。 - アプリ部門1位 🥇",
+          },
+          {
+            role: "インフラエンジニア",
+            company: "ちゅらデータ株式会社",
+            period: "2024年8月",
+            description:
+              "インターンシップでは、RAG（Retrieval-Augmented Generation）を活用して検索アプリケーションを開発しました。さらに、AWS、Snowflake、Terraform、dbtを使用してインフラを構築し、Snowsightを使ってデータの可視化を行いました。",
+          },
+          {
+            role: "データ分析",
+            company: "シンプルフォーム株式会社",
+            period: "2024年8月",
+            description:
+              "インターンシップでは、主にPythonを用いたデータ分析を行いました。データ分析の過程で必要となる要素についてチーム内で議論し、その結果をManagerに報告することで、アウトプットを共有しました。この経験を通じて、チーム全体での意思疎通やコミュニケーションの重要性を学びました。",
+          },
+        ],
       },
     },
     news: {
